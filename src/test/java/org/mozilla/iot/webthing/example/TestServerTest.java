80a60322cef81638356671c46c8a1d5bf7d40ecc
Tim Hinkes <timmeey@timmeey.de>
2018-03-25 15:51:32 +0200

    Updates package/file structure to java standards (#7)

---
+++ src/test/java/org/mozilla/iot/webthing/example/TestServerTest.java
@@ -0,0 +1,15 @@
+package org.mozilla.iot.webthing.example;
+
+import org.junit.Test;
+
+/**
+ * @author Tim Hinkes (timmeey@timmeey.de)
+ */
+public class TestServerTest {
+
+    @Test
+    public void testTestServer(){
+
+    }
+
+}
