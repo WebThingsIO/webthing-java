e27eeff7851a6ae9bc179be228dab37451dad532
Michael Stegeman <michael@stegeman.me>
2018-11-16 06:30:51 -0700

    Validate property values with JSON schema validator. (#35)

--- src/main/java/org/mozilla/iot/webthing/Property.java
+++ src/main/java/org/mozilla/iot/webthing/Property.java
@@ -5,6 +5,9 @@ package org.mozilla.iot.webthing;
 
 import com.google.common.collect.Lists;
 
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.mozilla.iot.webthing.errors.PropertyError;
@@ -72,105 +75,16 @@ public class Property<T> {
      * @throws PropertyError On validation error.
      */
     private void validateValue(T value) throws PropertyError {
-        if (this.metadata.has("type")) {
-            switch (this.metadata.getString("type")) {
-                case "null":
-                    if (!JSONObject.NULL.equals(value)) {
-                        throw new PropertyError("Value must be null");
-                    }
-                    break;
-                case "boolean":
-                    if (!(Boolean.FALSE.equals(value) ||
-                                  Boolean.TRUE.equals(value) ||
-                                  (value instanceof String &&
-                                           (((String)value).equalsIgnoreCase(
-                                                   "true") ||
-                                                    ((String)value).equalsIgnoreCase(
-                                                            "false"))))) {
-                        throw new PropertyError("Value must be a boolean");
-                    }
-                    break;
-                case "object":
-                    if (!(value instanceof JSONObject)) {
-                        throw new PropertyError("Value must be an object");
-                    }
-                    break;
-                case "array":
-                    if (!(value instanceof JSONArray)) {
-                        throw new PropertyError("Value must be an array");
-                    }
-                    break;
-                case "number":
-                    if (!(value instanceof Number)) {
-                        throw new PropertyError("Value must be a number");
-                    }
-                    break;
-                case "integer":
-                    if (!(value instanceof Integer)) {
-                        throw new PropertyError("Value must be an integer");
-                    }
-                    break;
-                case "string":
-                    if (!(value instanceof String)) {
-                        throw new PropertyError("Value must be a string");
-                    }
-                    break;
-            }
-        }
-
         if (this.metadata.has("readOnly") &&
                 this.metadata.getBoolean("readOnly")) {
             throw new PropertyError("Read-only property");
         }
 
-        if (this.metadata.has("minimum")) {
-            double minimum = this.metadata.getDouble("minimum");
-            double v = ((Number)value).doubleValue();
-
-            if (v < minimum) {
-                throw new PropertyError(String.format(
-                        "Value less than minimum: %f",
-                        minimum));
-            }
-        }
-
-        if (this.metadata.has("maximum")) {
-            double maximum = this.metadata.getDouble("maximum");
-            double v = ((Number)value).doubleValue();
-
-            if (v > maximum) {
-                throw new PropertyError(String.format(
-                        "Value greater than maximum: %f",
-                        maximum));
-            }
-        }
-
-        if (this.metadata.has("enum") && this.metadata.has("type")) {
-            List e = this.metadata.getJSONArray("enum").toList();
-
-            switch (this.metadata.getString("type")) {
-                case "number": {
-                    double v = ((Number)value).doubleValue();
-                    if (e.size() > 0 && !e.contains(v)) {
-                        throw new PropertyError("Invalid enum value");
-                    }
-                    break;
-                }
-                case "integer": {
-                    int v = ((Number)value).intValue();
-                    if (e.size() > 0 && !e.contains(v)) {
-                        throw new PropertyError("Invalid enum value");
-                    }
-                    break;
-                }
-                case "string": {
-                    String v = (String)value;
-                    if (e.size() > 0 && !e.contains(v)) {
-                        throw new PropertyError("Invalid enum value");
-                    }
-                    break;
-                }
-            }
+        Schema schema = SchemaLoader.load(this.metadata);
+        try {
+            schema.validate(value);
+        } catch (ValidationException e) {
+            throw new PropertyError("Invalid property value");
         }
     }
 
